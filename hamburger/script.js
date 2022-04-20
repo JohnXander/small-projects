06dc73219af1727b36fab493a3a8e6c8f3554ef9
John Xander <johnblox5@gmail.com>
2022-04-20 08:18:13 +0000

    js comments updated

--- hamburger/script.js
+++ hamburger/script.js
@@ -1,5 +1,9 @@
+// Store HTML elements
+
 const btn = document.getElementById("btn")
 const nav = document.getElementById("nav")
+
+// When button is clicked, a different style is activated for both elements
 
 btn.addEventListener("click", () => {
     nav.classList.toggle("active");
--- toast-notification/script.js
+++ toast-notification/script.js
@@ -1,17 +1,30 @@
+// Store HTML elements
+
 const btn = document.getElementById("btn");
 const container = document.getElementById("container");
+
+// Call function when button is clicked
 
 btn.addEventListener("click", () => {
     createNotification();
 });
 
+// Function which creates toast notification:
+
 function createNotification() {
+
+    // Create div element with style and message
+
     const notif = document.createElement("div");
     notif.classList.add("toast");
 
     notif.innerText = "Toast Notification!"
 
+    // Add above element to HTML file
+
     container.appendChild(notif);
+
+    // The element will disappear after 3 seconds
 
     setTimeout(() => {
         notif.remove();
